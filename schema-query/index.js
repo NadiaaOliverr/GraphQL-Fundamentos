a10e6e9f13e99d77f3060de4e9b672191ee569c7
nadiaaoliverr@gmail.com <nadia@DESKTOP-K9CI8PF.localdomain>
2020-11-12 00:06:22 -0300

    Adicionando primeira consulta

--- schema-query/index.js
+++ schema-query/index.js
@@ -1,1 +1,30 @@
-console.log('olá mundo')
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+    # Pontos de entradas da API
+    type Query {
+        ola: String
+        horaAtual: String
+    }
+`
+
+const resolvers = {
+
+    Query: {
+        ola() {
+            return "Olá mundo com GraphQL!"
+        },
+        horaAtual() {
+            return `${new Date()}`
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen().then(({ url }) => {
+    console.log(`Executando em ${url}`)
+});
