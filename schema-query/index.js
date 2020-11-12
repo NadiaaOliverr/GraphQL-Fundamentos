144216f9bb90e9cbd8f64d37f51b21ac501a97e4
nadiaaoliverr@gmail.com <nadia@DESKTOP-K9CI8PF.localdomain>
2020-11-12 03:15:35 -0300

    Adicionando resolvers

--- schema-query/index.js
+++ schema-query/index.js
@@ -3,21 +3,45 @@ const { ApolloServer, gql } = require('apollo-server')
 const typeDefs = gql`
     scalar Date
 
+    type Usuario {
+        id: ID
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
     # Pontos de entradas da API
     type Query {
-        ola: String
-        horaAtual: Date
+        ola: String!
+        horaAtual: Date!
+        usuarioLogado: Usuario
     }
 `
 
 const resolvers = {
-
+    Usuario: {
+        salario(usuario) {
+            return usuario.salario_real
+        }
+    },
     Query: {
         ola() {
             return "Olá mundo com GraphQL!"
         },
         horaAtual() {
             return new Date
+        },
+        usuarioLogado() {
+            return {
+                id: 1,
+                nome: 'Nádia Oliveira',
+                email: 'nadiaaoliverr@gmail.com',
+                idade: 21,
+                salario_real: 1234.56,
+                vip: true
+            }
         }
     }
 }
