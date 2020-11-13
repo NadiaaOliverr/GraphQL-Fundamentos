5c1efa641c75f59b719763188964d90f013847a2
nadiaaoliverr@gmail.com <nadia@DESKTOP-K9CI8PF.localdomain>
2020-11-13 10:43:23 -0300

    Adicionando Enum

--- schema-query/data/db.js
+++ schema-query/data/db.js
@@ -15,21 +15,24 @@ const usuarios = [
         nome: 'João Silva',
         email: 'jsilva@gmai..com',
         idade: 29,
-        perfil_id: 1
+        perfil_id: 1,
+        status: 'ATIVO'
     },
     {
         id: 2,
         nome: 'Rafael Júnior',
         email: 'rafajun@gmail.com',
         idade: 31,
-        perfil_id: 2
+        perfil_id: 2,
+        status: 'INATIVO'
     },
     {
         id: 3,
         nome: 'Daniela Junqueira',
         email: 'danijun@gmail.com',
         idade: 24,
-        perfil_id: 1
+        perfil_id: 1,
+        status: 'BLOQUEADO'
     }
 ]
 
