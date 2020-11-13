2961a1113eb0db69ae3d7a9508406dc24d127d1d
nadiaaoliverr@gmail.com <nadia@DESKTOP-K9CI8PF.localdomain>
2020-11-12 23:08:57 -0300

    Adicionando mais um resolver de arrays com parâmetro na consulta (sem destructuring)

--- schema-query/index.js
+++ schema-query/index.js
@@ -21,8 +21,24 @@ const usuarios = [
     }
 ]
 
+const perfis = [
+    {
+        id: 1,
+        nome: 'Comum'
+    },
+    {
+        id: 2,
+        nome: 'Administrador'
+    }
+]
+
 const typeDefs = gql`
     scalar Date
+
+    type Perfil {
+        id: Int
+        nome: String
+    }
 
     type Usuario {
         id: Int
@@ -49,6 +65,8 @@ const typeDefs = gql`
         numerosMegaSena: [Int!]!
         usuarios: [Usuario]
         usuario(id: Int): Usuario
+        perfis: [Perfil]
+        perfil(id: Int): Perfil
     }
 `
 
@@ -104,6 +122,13 @@ const resolvers = {
         usuario (_, { id }) {
             const selecionados = usuarios.filter(u => u.id === id)
             return selecionados ? selecionados[0] : null
+        },
+        perfis () {
+            return perfis
+        },
+        perfil (_, args) {
+            const selecionados = perfis.filter(p => p.id == args.id)
+            return selecionados ?  selecionados[0] : null
         }
     }
 }
