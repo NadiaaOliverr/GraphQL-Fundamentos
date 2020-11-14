77dee16edd560f7331149149348ef99a84c4e4d8
NadiaaOliver <nadia@DESKTOP-K9CI8PF.localdomain>
2020-11-14 14:35:07 -0300

    Adicionando validação de e-mail existente

--- mutation/resolvers/Mutation.js
+++ mutation/resolvers/Mutation.js
@@ -1,12 +1,19 @@
 const { usuarios, proximoId } = require('../data/db')
 
 module.exports = {
-    novoUsuario(_, {nome, email, idade}) {
+    // {nome, email, idade}
+    novoUsuario(_, args) {
+        const emailExistente = usuarios.some(
+            u => u.email === args.email
+        )
+
+        if(emailExistente) {
+            throw new Error('E-mail cadastrado')
+        }
+
         const novo = {
             id: proximoId(),
-            nome,
-            email,
-            idade,
+            ...args,
             perfil_id: 1,
             status: 'ATIVO'
         }
